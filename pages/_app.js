1fe1323b4d66cdd28fe2f09967d044e379ab1da8
raisoturu <anvaqta@student.telkomuniversity.ac.id>
2020-12-23 13:49:42 +0700

    add nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,16 @@ import Footer from "@/components/Footer";
 import { ChakraProvider, Stack } from "@chakra-ui/react";
 import Navbar from "../components/Navbar";
 import theme from "theme";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 import "../styles/globals.css";
 import { AuthProvider } from "@/utils/AuthContext";
+import Router from "next/router";
 
+NProgress.configure({ showSpinner: false });
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
