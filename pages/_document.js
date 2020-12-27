8b3096e7f9dc4a95e4779a32bdd6dde476c495d8
Anvaqta Tangguh Wisesa <anvaqta@student.telkomuniversity.ac.id>
2020-12-27 22:42:31 +0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,8 @@ import { ColorModeScript } from "@chakra-ui/react";
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 
 class Document extends NextDocument {
-  static async getInitialProps(ctx) {
-    const initialProps = await NextDocument.getInitialProps(ctx);
-    return { ...initialProps };
+  static getInitialProps(ctx) {
+    return NextDocument.getInitialProps(ctx)
   }
 
   render() {
